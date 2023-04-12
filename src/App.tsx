512e1ce71701e2a06fb6eb58df55230fa8fad0d8
JW Desktop <wlsdhks5495@naver.com>
2023-04-12 23:08:20 +0900

    홈 화면 문구 수정

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Berrycracker
+          Berrycracker5
         </a>
       </header>
     </div>
